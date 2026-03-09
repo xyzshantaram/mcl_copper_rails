afafbe8ff6bf9fbca42b568b1490d4e603fb75eb
Siddharth Singh <me@shantaram.xyz>
2026-03-09 20:21:46 +0530

    add recipes of activator and detector rails

--- init.lua
+++ init.lua
@@ -15,3 +15,21 @@ core.register_craft({
 		{ "mcl_copper:copper_ingot", "mesecons:redstone", "mcl_copper:copper_ingot" },
 	}
 })
+
+core.register_craft({
+	output = "mcl_minecarts:detector_rail_v2 6",
+	recipe = {
+		{ "mcl_copper:copper_ingot", "",                              "mcl_copper:copper_ingot" },
+		{ "mcl_copper:copper_ingot", "mesecons_pressureplates:pressure_plate_stone_off", "mcl_copper:copper_ingot" },
+		{ "mcl_copper:copper_ingot", "mesecons:redstone",            "mcl_copper:copper_ingot" },
+	}
+})
+
+core.register_craft({
+	output = "mcl_minecarts:activator_rail_v2 6",
+	recipe = {
+		{ "mcl_copper:copper_ingot", "mcl_core:stick", "mcl_copper:copper_ingot" },
+		{ "mcl_copper:copper_ingot", "mesecons_torch:mesecon_torch_on", "mcl_copper:copper_ingot" },
+		{ "mcl_copper:copper_ingot", "mcl_core:stick", "mcl_copper:copper_ingot" },
+	}
+})
